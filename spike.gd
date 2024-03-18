46448fc73c29e05a9a311d6035913941740ae5ac
dsantiago19 <dsantiago19@fordham.edu>
2024-03-18 14:45:10 -0400

    UPDATE GAME

--- spike.gd
+++ spike.gd
@@ -5,5 +5,8 @@ func _on_spikes_body_entered(body):
 
 
 func _on_body_entered(body):
-
+	call_deferred("respawn")
+	
+func respawn():
 	get_tree().change_scene_to_file("res://DESIGN SCENE.tscn")
+	print("testing")
