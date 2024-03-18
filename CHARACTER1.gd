55534eb3d318fe8d589d702a07c2f5415065af7e
Kdiaz48 <kdiaz48@fordham.edu>
2024-03-18 12:13:50 -0400

    fixed character and finished menu

--- CHARACTER1.gd
+++ CHARACTER1.gd
@@ -14,24 +14,18 @@ func _physics_process(delta):
 func move():
 	if Input.is_action_pressed("right"):
 		position.x += 5
-	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.flip_h = false
+		animation.play("RUN")
+	elif Input.is_action_pressed("left"):
 		position.x -= 5
-		
+		$AnimatedSprite2D.flip_h = true
+		animation.play("RUN")
+	else: 
+		animation.play("IDLE")
 	velocity.y += gravity
 	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
 		velocity.y = JUMP_POWER
 	move_and_slide()
-	
-	if "KEY_RIGHT": 
-		$AnimatedSprite2D.flip_h = false
-		animation.play("run")
-	elif "KEY_LEFT":
-		$AnimatedSprite2D.flip_h = true
-		animation.play("run")
-	else:
-		animation.play("idle")
-		
-		
 	
 
 
